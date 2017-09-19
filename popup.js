bb3d4c2bcb8ad40fe595524d0300784e7ab1ec8e
Miguel <hisrubio51@gmail.com>
2017-09-19 17:42:43 +0200

    chrome extension that says the url of your actual tab

---
+++ popup.js
@@ -0,0 +1,12 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+	chrome.tabs.query({
+	    active: true,
+	    lastFocusedWindow: true
+	}, function(tabs) {
+	    // and use that tab to fill in out title and url
+	    var tab = tabs[0];
+	    console.log(tab.url);
+	    alert(tab.url);
+	});
+});
