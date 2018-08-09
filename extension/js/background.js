52a0e495d89859a212493a80632155e63181f28b
Miguel <hisrubio51@gmail.com>
2018-08-09 19:23:24 +0200

    teclas de acceso rapido

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,4 +1,13 @@
 
+chrome.commands.onCommand.addListener(function(command) {
+	if(command == "apagar"){
+		window.open("http://192.168.1.42:80/led=0");
+    }
+    if(command == "encender"){
+		window.open("http://192.168.1.42:80/led=1");
+    }
+
+});
 
 var pages = [
     /www.netflix.com/i,
